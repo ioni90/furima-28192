ee5c36219778a21c2eabbdf1f827bb92b3c782e3
ioni90 <kkoioni27@gmail.com>
2020-09-19 20:47:12 +0900

    バリデーション修正３、deviseの不要なコメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,24 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  full_width_char = '\A[ぁ-んァ-ン一-龥]+\z'
+  kana_char = '\A[ァ-ン]+\z'
 
   ## ユーザー情報
-  validates :nickname, presence: true
-  validates :email, presence: true,
-                    uniqueness: true,
-                    format: { with: /@.+/ }
-  validates :password, presence: true, length: { minimum: 6 },
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true,
+                      format: { with: /@.+/ }
+    validates :password, length: { minimum: 6 },
                        format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :password_confirmation, presence: true
+    validates :password_confirmation
 
   ## 本人情報確認
-  validates :first_name, presence: true,
-                         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :last_name, presence: true,
-                        format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name_kana, presence: true,
-                              format: { with: /\A[ァ-ン]+\z/ }
-  validates :last_name_kana, presence: true,
-                             format: { with: /\A[ァ-ン]+\z/ }
-  validates :birthday, presence: true
+    validates :first_name, format: { with: /#{full_width_char}/ }
+    validates :last_name, format: { with: /#{full_width_char}/ }
+    validates :first_name_kana, format: { with: /#{kana_char}/ }
+    validates :last_name_kana, format: { with: /#{kana_char}/ }
+    validates :birthday
+  end
 end
