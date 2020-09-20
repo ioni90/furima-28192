657532005197fbbedae30fa7a7d2ab4dfe50087d
ioni90 <kkoioni27@gmail.com>
2020-09-20 22:08:05 +0900

    ログイン後のユーザーだけが出品ページに遷移できる、ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_detail, :category_id, 
+                                 :item_condition_id, :delivery_fee_id, 
+                                 :prefecture_id, :shipping_date_id, 
+                                 :price).merge(user_id: current_user.id)
+  end
+
 end
