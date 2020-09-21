42635002018acf02eb47537ee27f9552139d080b
ioni90 <kkoioni27@gmail.com>
2020-09-21 17:27:35 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_date
+
 end
