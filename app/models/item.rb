2f7856d2fa86c5fb1dbfd2c76857025fcb4c07c8
ioni90 <kkoioni27@gmail.com>
2020-09-24 21:06:46 +0900

    itemモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,5 +7,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_date
+  
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_detail
+    validates :price, numericality: { only_integer: true, 
+                                      greater_than: 299, less_than: 10000000 }
+  end
 
+  with_options presence: true, numericality: { other_than: 0 } do
+    validates :category_id
+    validates :item_condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
 end
