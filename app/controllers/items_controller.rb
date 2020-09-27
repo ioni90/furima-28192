370a2c2bae7d273c475e1762278b1199693ac7a5
ioni90 <kkoioni27@gmail.com>
2020-09-27 14:55:09 +0900

    Rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -17,6 +17,7 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_params
     params.require(:item).permit(:image, :item_name, :item_detail, :category_id,
                                  :item_condition_id, :delivery_fee_id,
