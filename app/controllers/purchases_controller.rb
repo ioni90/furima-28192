6767de76a9131ab484420ed319e44b9ed2ef1829
ioni90 <kkoioni27@gmail.com>
2020-10-04 17:49:31 +0900

    商品購入画面用にコントローラーの設定

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,47 @@
+class PurchasesController < ApplicationController
+  before_action :select_item, only: [:index, :create, :pay_item]
+  before_action :authenticate_user!
+
+  def index
+    unless Purchase.exists?(item_id: @item.id)
+      @purchase = PurchaseShipToLocation.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @purchase = PurchaseShipToLocation.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_ship_to_location).permit(:token, :postal_code, 
+                                                      :prefecture_id, :city, 
+                                                      :address, :building, 
+                                                      :phone_num, 
+                                                      ).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
+  end
+
+  def select_item
+    @item = Item.find(params[:item_id])
+  end
+
+end
